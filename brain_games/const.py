4eba648121642e0050ebd20e68859668c13eba7b
VictorVangeli <whgaleon1@yandex.ru>
2024-09-19 17:25:10 +0300

    add "const.py"

---
+++ brain_games/const.py
@@ -0,0 +1,18 @@
+"""This file include constant\'s for Brain Games."""
+
+"""Block with game rules"""
+
+CALC_RULES = 'What is the result of the expression?'
+EVEN_RULES = 'Answer "yes" if the number is even, otherwise answer "no".'
+GCD_RULES = 'Find the greatest common divisor of given numbers.'
+PRIME_RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+PROGRESSION_RULES = 'What number is missing in the progression?'
+
+"""Block with utility text for output"""
+
+GREETING_MESSAGE = 'Welcome to the Brain Games!'
+ENTER_NAME = "May I have your name? "
+
+"""Block with constant for game logic"""
+
+ROUNDS = 3
