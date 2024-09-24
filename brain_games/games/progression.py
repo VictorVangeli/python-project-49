0bb0009efc9b6712529c0bb57f03d94cf33a613d
VictorVangeli <whgaleon1@yandex.ru>
2024-09-24 13:43:58 +0300

    update 'progression.py'

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,7 +1,6 @@
-import random
 
 from brain_games.const import PROGRESSION_RULES
-from brain_games.games.engine import ask_question, play_game
+from brain_games.games.engine import ask_question, play_game, get_random_number
 
 
 def start_progression_game():
@@ -16,13 +15,13 @@ def progression_round(username: str) -> bool:
         tuple: Кортеж из строки с прогрессией (где один элемент заменен на '..')
          и правильного ответа.
     """
-    start = random.randint(1, 100)
-    step = random.randint(1, 40)
-    length = random.randint(5, 10)
+    start = get_random_number(1, 100)
+    step = get_random_number(1, 40)
+    length = get_random_number(5, 10)
 
     progression_list = [start + step * i for i in range(length)]
 
-    hide_elements = random.randint(0, len(progression_list) - 1)
+    hide_elements = get_random_number(0, len(progression_list) - 1)
     correct_answer = str(progression_list[hide_elements])
     progression_list[hide_elements] = ".."
     question = " ".join(map(str, progression_list))
