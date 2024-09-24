63909f406fe02b491e778526d66e644aeb2ff4f0
VictorVangeli <whgaleon1@yandex.ru>
2024-09-24 11:56:00 +0300

    move 'cli.py', update func 'welcome_user()'

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,10 @@
+import prompt
+
+
 def welcome_user():
     """
     Приглашает пользователя ввести свое имя, а затем приветствует его по имени.
     """
-    name = ""
-    while not name.strip():
-        name = input("May I have your name? ")
+    name = prompt.string('May I have your name? ')
     print(f"Hello, {name}!")
     return name
