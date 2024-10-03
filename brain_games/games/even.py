64dfe308b919fd9d5380e817361adb6e478c16bf
VictorVangeli <whgaleon1@yandex.ru>
2024-10-03 10:55:21 +0300

    update 'even.py'

--- brain_games/games/even.py
+++ brain_games/games/even.py
@@ -1,10 +1,6 @@
 from brain_games.const import EVEN_RULES
-from brain_games.games.engine import ask_question, play_game
+from brain_games.games.engine import play_game
 from brain_games.utils import get_random_number
-
-
-def start_even_game():
-    play_game(run_even_round, EVEN_RULES)
 
 
 def is_even(number: int) -> bool:
@@ -20,7 +16,7 @@ def is_even(number: int) -> bool:
     return number % 2 == 0
 
 
-def run_even_round(username: str) -> bool:
+def run_even_round() -> tuple[str, str]:
     """
     Проводит один раунд игры, в котором игрок должен угадать, является ли число
     четным.
@@ -29,12 +25,16 @@ def run_even_round(username: str) -> bool:
     четное, и 'no', если нечетное. Если игрок отвечает правильно, раунд
     считается выигранным.
 
-    Args:
-        username (str): Имя пользователя
 
     Returns:
-        bool: True, если игрок дал правильный ответ, False в случае ошибки.
+        tuple[str, str]: Кортеж, содержащий вопрос в виде строки и правильный
+                         ответ в виде строки.
     """
     number = get_random_number()
+    question = str(number)
     correct_answer = "yes" if is_even(number) else "no"
-    return ask_question(f"{number}", correct_answer, username)
+    return question, correct_answer
+
+
+def start_even_game():
+    play_game(run_even_round, EVEN_RULES)
